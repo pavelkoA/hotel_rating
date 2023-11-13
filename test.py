85c1a109172bbde57e65341fadaa84a8b4eb6311
pavelkoA <115475612+pavelkoA@users.noreply.github.com>
2023-11-13 16:09:32 +0300

    test
    test

--- test.py
+++ test.py
@@ -1,26 +1,19 @@
-import random
-
-import requests
-from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
-from get_rating.headers_and_proxies import proxy_list
 from get_rating.csv_utils import read_csv_file, write_csv_file
 from get_rating.rating_utils import get_level_travel_rating
 
-ua = UserAgent()
+def main():
+    hotel_leaveltravel_dict = read_csv_file("get_rating/docs/read_file/leaveltravel.csv")
+    
+    count = 0
+    for hotel in hotel_leaveltravel_dict:
+        hotel["rating"] = get_level_travel_rating(hotel["URL"])
+        count += 1
+        print(f"{count}) {hotel['URL']} - {hotel['rating']}")
+    write_csv_file(hotel_leaveltravel_dict)
 
 
-header = {
-    "User-Agent": ua.random
-}
-
-hotel_leaveltravel_dict = read_csv_file("get_rating/docs/read_file/leaveltravel.csv")
-
-count = 0
-for hotel in hotel_leaveltravel_dict:
-    hotel["rating"] = get_level_travel_rating(hotel["URL"])
-    count += 1
-    print(f"{count}) {hotel['URL']} - {hotel['rating']}")
-write_csv_file(hotel_leaveltravel_dict)
+if __name__=="__main__":
+    main()
 
 
